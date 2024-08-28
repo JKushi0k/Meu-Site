8d179609ef0d1bf0ff89bfc592251bb5e4789b0d
JKushu0k <asta56221@gmail.com>
2024-08-28 10:13:42 -0300

    adicionando vídeos

--- js/video.js
+++ js/video.js
@@ -49,6 +49,8 @@ document.addEventListener('DOMContentLoaded', () => {
         sidebar.classList.toggle('expandir')
     })
 
+    
+
     // Aparecer vídeos do Youtube
     const apiKey    = 'AIzaSyACfwGE-Qb25AUHBp99cv4jNLuwCtYo2y4'
     const channelId = 'UCEu8sLYdIu4WRozw-Pdt4mA'
@@ -72,11 +74,76 @@ document.addEventListener('DOMContentLoaded', () => {
         try {
             const response = await fetch(apiUrl)
             const data = await response.json()
+            console.log('Dados da API:', data)
             videos = data.items
 
+            // Verificar se os vídeos foram obtidos corretamente
+            if (!videos || videos.length == 0) {
+                console.error('Nenhum vídeo encontrado');
+                return;
+            }
 
+            // Armazena os vídeos e a hora atual do cache
+            localStorage.setItem('videos', JSON.stringify(videos))
+            localStorage.setItem('cacheTime', new Date().getTime())
+
+            currentIndex = 0
+            displayVideos()
         } catch (error) {
-
+            console.error('Erro ao buscar vídeos:', error)
         }
     }
+
+    // Função para exibir vídeos em lotes de 10
+    function displayVideos() {
+        const videoContainer = document.getElementById('videoContainer')
+
+        if (!videoContainer) {
+            console.error('Contêiner de vídeos não encontrado no HTML.');
+            return;
+        }
+        console.log('Exibindo vídeos:', videos.slice(currentIndex, currentIndex + batchSize));
+
+        // Carregar o próximo lote de vídeos
+        for (let i = currentIndex; i < currentIndex + batchSize && i < videos.length; i++) {
+            const video = videos[i]
+            const videoId = video.id.videoId
+            const videoTitle = video.snippet.title
+            const videoThumbnail = video.snippet.thumbnails.medium.url // URL da imagem de pré-visualização
+
+            const videoEmbed = `
+                <div class="video">
+                    <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
+                        <img src="${videoThumbnail}" alt="${videoTitle}">
+                        <h3>${videoTitle}</h3>
+                    </a>
+                </div>
+            `
+            videoContainer.innerHTML += videoEmbed
+        }
+
+        currentIndex += batchSize
+
+        // Esconder o botão "Carregar Mais" se não houver mais vídeos
+        if (currentIndex >= videos.length) {
+            document.getElementById('loadMore').style.display = 'none'
+        }
+    }
+
+    // Verifeica o cache e decide se busca da API ou do cache
+    function loadVideos() {
+        if (isCacheValid()){
+            videos = JSON.parse(localStorage.getItem('videos'))
+            currentIndex = 0
+            displayVideos()
+        }else {
+            fetchVideos()
+        }
+    }
+
+    // Event Listener para o botão "Carregar Mais"
+    document.getElementById('loadMore').addEventListener('click', displayVideos)
+
+    // Carrega os vídeos ao carregar a página
+    document.addEventListener('DOMContentLoaded', loadVideos);
 })
